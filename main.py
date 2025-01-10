9580f1b4450142fd7c8734483d7faec06a9ab066
Gerardo Martinez Alvarez <diryess@gmail.com>
2025-01-09 23:41:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI del Gera Gera!"}
+    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI del Gera Gera otro!"}

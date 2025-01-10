0741eb4a82459980e7b9eebfd4c53984e91e5f7e
Gerardo Martinez Alvarez <diryess@gmail.com>
2025-01-09 23:50:07 -0600

    cors

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Configuración del CORS para permitir solo lectura desde cualquier origen
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite cualquier origen
+    allow_credentials=True,
+    allow_methods=["GET", "OPTIONS"],  # Solo permite métodos GET y OPTIONS (lectura)
+    allow_headers=["*"],  # Permite todas las cabeceras
+)
+
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI del Gera Gera otro!"}
+    return {"greeting": "Hello, World!", "message": "¡Bienvenido a la API de solo lectura!"}
